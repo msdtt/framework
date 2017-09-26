8e782ddfb6a77f130eddb978ddb7d899f49b5b2e
竺旭栋 <zhuxd@wjs.com>
2017-09-26 15:48:39 +0800

    初始化

---
+++ src/main/java/com/msdtt/framework/Application.java
@@ -0,0 +1,37 @@
+package com.msdtt.framework;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+/**
+ * @author zhuxd@wjs.com
+ * @date 2017/8/12 2:15
+ */
+@Controller
+@EnableWebMvc
+@SpringBootApplication
+//@MapperScan(basePackages = "com.ads.mapper")
+public class Application extends SpringBootServletInitializer{
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    String home() {
+        return "redirect:index";
+    }
+
+}
